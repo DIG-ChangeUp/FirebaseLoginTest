a01192031d75e80d8668668e320fc575e015eca8
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-14 22:09:20 +0900

    mailとpassword認証設定

--- frontend/src/component/SingUp.tsx
+++ frontend/src/component/SingUp.tsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
 
-const SignUp:React.FC = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-        event.preventDefault();
-        console.log('登録');
-    };
+import { auth } from '../auth/firebase.ts';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-    return (
+const SignUp: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+
+    console.log('email:', email, '  /password:', password);
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log('User created:', userCredential.user);
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
+  };
+
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>ユーザ登録</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>メールアドレス</label>
-                    <input name="email" type="email" placeholder="email" />
-                </div>
-                <div>
-                    <label>パスワード</label>
-                    <input name="password" type="password" />
-                </div>
-                <div>
-                    <button>登録</button>
-                </div>
-            </form>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleChangeEmail(event)
+            }
+          />
         </div>
-    );
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleChangePassword(event)
+            }
+          />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
