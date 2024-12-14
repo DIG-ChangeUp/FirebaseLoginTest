a21c06ffc3fcb26d72ecd6ed0b729b08010c015a
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-14 20:45:03 +0900

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import SignUp from './component/SingUp.tsx';
+
+function App() {
+  return (
+    <>
+      <h1>Firebase auth sample!</h1>
+      <SignUp />
+    </>
+  );
+}
+
+export default App;
