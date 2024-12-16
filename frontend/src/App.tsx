48868a980b1204c31f1c2a8206fa683b10643700
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-16 09:09:35 +0900

    Home,Loginページの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import SignUp from './component/SingUp.tsx';
 import { AuthProvider } from './auth/authContext.tsx';
 import { Route, Routes } from 'react-router-dom';
+import Home from './component/Home.tsx';
+import Login from './component/Login.tsx';
 
 function App() {
   return (
@@ -8,7 +10,9 @@ function App() {
       <h1>Firebase auth sample!</h1>
       <AuthProvider>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/singup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </AuthProvider>
     </>
